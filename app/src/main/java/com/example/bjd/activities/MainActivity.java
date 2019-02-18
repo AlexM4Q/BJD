6431b37e753a0db190cce44eb98099582ec2fa91
zhelonkin <Zhelonkin.ru@gmail.com>
2019-02-18 23:42:58 +0400

    Релиз

--- app/src/main/java/com/example/bjd/activities/MainActivity.java
+++ app/src/main/java/com/example/bjd/activities/MainActivity.java
@@ -18,6 +18,9 @@ import lombok.val;
 
 public final class MainActivity extends AppCompatActivity {
 
+    private EditText searchQuery;
+    private LinearLayout searchResult;
+
     private Subjects subjects;
 
     @Override
@@ -27,38 +30,47 @@ public final class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         subjects = SubjectsReader.read(this);
-
-        @NotNull val inflater = LayoutInflater.from(this);
-        @NotNull final EditText searchQuery = findViewById(R.id.searchQuery);
-        @NotNull final LinearLayout searchResult = findViewById(R.id.searchResult);
+        searchQuery = findViewById(R.id.searchQuery);
+        searchResult = findViewById(R.id.searchResult);
 
         searchQuery.setOnEditorActionListener((v, actionId, event) -> {
-            @NotNull val query = searchQuery.getText().toString();
-            if (query.length() > 3) {
-                searchResult.removeAllViews();
-
-                @NotNull val questions = subjects.findQuestions(query);
-
-                for (@NotNull val question : questions) {
-                    @NotNull val layout = (TextView) inflater.inflate(R.layout.item_search_result, null, false);
-
-                    @NotNull val builder = new StringBuilder(question.getName());
-                    for (@NotNull val answer : question.getAnswers()) {
-                        builder.append("\n ");
-                        builder.append(answer.isRight());
-                        builder.append(' ');
-                        builder.append(answer.getName());
-                    }
-
-                    builder.append("\n\n");
-
-                    layout.setText(builder.toString());
-                    searchResult.addView(layout);
-                }
-            }
-
+            search();
             return true;
         });
+
+        findViewById(R.id.searchAction).setOnClickListener(v -> search());
+    }
+
+    private void search() {
+        @NotNull val inflater = LayoutInflater.from(this);
+        @NotNull val query = searchQuery.getText().toString();
+
+        if (query.equalsIgnoreCase("Антон")) {
+            searchQuery.setText("Мудрости пропогандон");
+        }
+
+        if (query.length() > 3) {
+            searchResult.removeAllViews();
+
+            @NotNull val questions = subjects.findQuestions(query);
+
+            for (@NotNull val question : questions) {
+                @NotNull val layout = (TextView) inflater.inflate(R.layout.item_search_result, null, false);
+
+                @NotNull val builder = new StringBuilder(question.getName());
+                for (@NotNull val answer : question.getAnswers()) {
+                    builder.append("\n ");
+                    builder.append(answer.isRight());
+                    builder.append(' ');
+                    builder.append(answer.getName());
+                }
+
+                builder.append("\n\n");
+
+                layout.setText(builder.toString());
+                searchResult.addView(layout);
+            }
+        }
     }
 
 }
